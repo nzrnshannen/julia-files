510dc8291e0b9cbd2a62a81a365b5c057341e2e4
Shannen Nazareno <nzrnshannen@gmail.com>
2024-03-25 03:28:05 +0800

    added boolean basics

--- Julia Basics/3_bool.jl
+++ Julia Basics/3_bool.jl
@@ -1,2 +1,48 @@
+###############################################################################################
 # BOOLEAN 
+# true or false
 
+                # COMPARISON OPERATORS (compares numerical values) #
+    # NOTE: Shift + Enter and place cursor on the line of code to see result
+
+# < (less than)
+3 < 2
+
+# > (greater than)
+5 > 0 
+
+# >= (greater than or equal to)
+3 >= 3 
+
+# <= (less than or equal to)
+9 <= 10 
+
+# == (equality operator)
+4 == 3 
+
+# != (inequality operator)
+4 != 7 
+
+
+                # BOOLEAN OEPRATORS #
+
+# Boolean AND (&)
+# Boolean OR (|)
+# short-circuiting Boolean AND (&&)
+# short-circuiting Boolean OR (||)
+
+
+#EXAMPLES
+
+# && short-circuiting AND
+1 == 1 && 1 > 1
+# true AND false = false
+
+# || short-circuiting OR 
+1 == 1 || 1 > 1
+# true OR false = true
+# 1 > 1 won't be evaluated as 1 == 1 is already true
+
+# true = 1, false = 0
+false + true + true 
+(true + true + true)^false
